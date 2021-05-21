01cac5496a7f2b637515659817fe8f07d3a14c6b
swhan9404 <swhan9404@naver.com>
2021-05-22 01:02:05 +0900

    메인페이지 slider 작동

---
+++ src/components/MoviesSliderItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img class="swiper-slide" :src="imgSrc"/> -->
+  <div class="swiper-slide"></div>
+</template>
+
+<script>
+export default {
+  name : 'MoviesSliderItem',
+  props : {
+    movie : {
+      type : Object
+    }
+  },
+  computed : {
+    imgSrc : function(){
+      return "https://image.tmdb.org/t/p/w500"+this.movie.poster_path
+    },
+    styleSwiper : function(){
+      return "background-image:url("+this.imgSrc+")"
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
-    Movies
+
+    <MoviesSlider :movies="latest_movies"/>
   </div>
 </template>
 
 <script>
+import MoviesSlider from '@/components/MoviesSlider.vue'
+
+import axios from 'axios'
+
+const BACKEND = process.env.VUE_APP_BACKEND_LINK
 export default {
+  
   name : 'Movies',
+  components : {
+    MoviesSlider,
+
+  },
+  data : function(){
+    return {
+      latest_movies : [],
+      highscore_movies : [],
+      like_movies : [],
+    }
+  },
+  methods : {
+    getLatestMovies : function(){
+      const link = 'movies'
+      axios.get(BACKEND+link)
+        .then(res =>{
+          this.latest_movies = res.data.latest_movies
+          this.highscore_movies = res.data.highscore_movies
+          this.like_movies = res.data.like_movies
+        })
+    }
+  },
+  created : function(){
+    this.getLatestMovies()
+  }
 }
 </script>
 
